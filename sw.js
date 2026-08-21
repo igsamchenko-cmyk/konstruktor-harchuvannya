237a4c8e0fe9d02aa06f91f1162a837c3ed485da
igsamchenko-cmyk <igsamchenko@gmail.com>
2026-08-21 09:16:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker: офлайн-робота додатку.
-   При оновленні index.html підвищуйте номер версії кешу (v9 → v10), щоб
+   При оновленні index.html підвищуйте номер версії кешу (v10 → v11), щоб
    користувачі отримали свіжу версію. */
-const CACHE = 'nutri-konstruktor-v9';
+const CACHE = 'nutri-konstruktor-v10';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
